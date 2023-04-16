2c7bab9c48fe389a1af23e48288dbf6acea7e9e5
Jessica Yun <jessicasopark@gmail.com>
2023-04-16 16:40:55 -0700

    added save function and function to store into local storage

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-$(document).ready(function () {
-    var timeDisplayEl = $('#date-display');
-    var hourEl = $('#hours');
+var eventsData;
+var timeDisplayEl = $('#time-display');
 
-    function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY');
-    timeDisplayEl.text(rightNow);
+var now = dayjs();
+$('#currentDay').text(now.format('MMM D, YYYY'));
+
+function setHourColors() {
+    for (var i = 9; i < 18; i++) {
+        if (i < now.hour()) {
+            $("#hour-" + i + " textarea").addClass("past");
+        } else if (i == now.hour()) {
+            $("#hour-" + i + " textarea").addClass("present");
+        } else if (i > now.hour()) {
+            $("#hour-" + i + " textarea").addClass("future");
+        }
     }
+}
 
-    // once clicked, event is saved on calendar
-    var saveButton = document.querySelector(".saveBtn");
-    saveButton.addEventListener("click", function(event){
-    alert("bye");
-    });
+function loadStoredData() {
+    eventsData = JSON.parse(localStorage.getItem("calendarEvents"));
+    if (!eventsData) {
+        eventsData = {
+            hour9:"",
+            hour10:"",
+            hour11:"",
+            hour12:"",
+            hour1:"",
+            hour2:"",
+            hour3:"",
+            hour4:"",
+            hour5:"",
+        };
+    }
+    //TODO load existing data from local storage
+}
 
-    // once clicked, calendar is cleared
-    var clearButton = document.querySelector(".clearBtn");
-    clearButton.addEventListener("click", function(event){
-    alert("hi");
-    });
+function handleSaveClick(event) {
+    // grab data from HTML
+    var hourBlock = $(event.target).parent();
+    var value = hourBlock.children("textarea").val();
+    var hour = hourBlock.attr('id').split("-")[1];
+    
+    // modify our data object
+    eventsData["hour" + hour] = value;
 
+    // store in local storage
+    localStorage.setItem("calendarEvents", JSON.stringify(eventsData));
+}
 
+$('.saveBtn').on('click', handleSaveClick);
+
+$(function() {
+    loadStoredData();
+    setHourColors();
 });
